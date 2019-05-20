fd5f3333bf4fc912c7a18fab5397fa4870e16bba
Mr54N664 <50640066+Mr54N664@users.noreply.github.com>
2019-05-20 10:35:58 +0700

    Add files via upload

---
+++ MR.54N664.sh
@@ -0,0 +1,191 @@
+#!/bin/sh
+
+Cy='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+e='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+              figlet GUNAKAN DENGAN BENAR | lolcat
+
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•" | lolcat
+echo  "|•                ""By  : Mr.54N664""                 |•" | lolcat
+echo  "|•          ""Wa  : 0888936xxxx privasi:v""           |•" | lolcat
+echo  "|•      ""Fb  : https://m.facebook.com/punk.3999""    |•" | lolcat
+echo  "|•      ""github : https://github.com/******:v""      |•" | lolcat
+echo  "|•   ""      YT = RezkyId23 subscribe sem***""        |•" | lolcat
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•" | lolcat
+
+          figlet JANGAN LUPA   SUBSCRIBE | lolcat
+
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+echo $me"1.        "$i"HACK WIFI (ROOT)""             √"
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+echo $me"2.         "$i"VIRTEXT GANAS!""              √"
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+echo $me"3.            "$i"SQLMAP""                   √"
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+echo $me"4.         "$i"HAMMERING DDOS""              √"
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+echo $me"5.     "$i"PENETRATION Test R_HAWK""         √"
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+echo $me"6.      "$i"TOOL HACK FB LENGKAP""           √"
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+echo $me"7.   "         $i"DEFACE""                   √"
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+echo $me"8.            "$i"WEEMAN""                   √"
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+echo $me"9. "$i"INSTAGRAM (FOLLOWING BELOM JADI)""    √"
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+echo $me"10. "$i"INSTAL BAHAN DULU (DISARANKAN)""     √"
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+echo $me"0.           "$i"EXIT/KELUAR""               √"
+echo  "|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+echo ""
+echo     "╭>⬇⬇⬇" |lolcat
+read -p"Pilih Angka==> " pil;
+
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x *
+sh wifi-hacker.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/muhammadfathul/VIRTEX
+cd VIRTEX
+sh virtex.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python sqlmap.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/cyweb/hammer
+cd hammer
+python3 hammer.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sh new.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/404rgr/webdav77
+cd webdav77
+chmod +x *
+./install.sh
+bash webdav77.sh
+fi
+
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/ikiganteng/bot-igeh
+cd bot-igeh
+chmod 777 node_modules.zip
+unzip node_modules.zip
+node index.js
+fi
+
+if [ $pil = 10 ]
+then
+clear
+pkg install unzip
+pkg install php
+pkg install nodejs
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip install requests
+pip2 install requests
+pip install mechanize
+pkg install unstable-repo
+pip2 install mechanize
+pkg install curl
+pkg install openssh
+pkg install php
+pjg install python
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $pil = 0 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $pur"Terima Kasih Sudah Pakai Tool Saya"
+sleep 2
+echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
+sleep 2
+echo $pur"WhatsApp :"$pur" +62888936XXXX"
+sleep 2
+echo $i">> Thanks Yang Sudah Support Saya <<"
+exit
+fi
+
